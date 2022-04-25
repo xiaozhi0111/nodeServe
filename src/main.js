4575a53f19a798d497127f52fbd133adc9b27bc6
unknown <zhaoshuaige>
2022-04-25 11:45:52 +0800

    添加路由

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ const Koa = require('koa');
 
 const {APP_PORT} = require('../config/config.default.js');
 
+const userRouter = require('./router/user.route');
+
 const app = new Koa();
 
-// app.get('/',(ctx,next)=>{
-//     ctx.body = 'hello'
-// })
+app.use(userRouter.routes());
 
 app.listen(APP_PORT,()=>{
     console.log(`server is run http:localhost:${APP_PORT}`);
